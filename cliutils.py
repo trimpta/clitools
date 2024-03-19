b150f4b13edbe9ecb50dd75ed69079f77b7658f1
Trimpta <68428753+trimpta@users.noreply.github.com>
2024-03-19 11:08:36 +0530

    fix #1 i think
    moved inbuilt to cliutils.py

--- cliutils.py
+++ cliutils.py
@@ -10,6 +10,35 @@ Cliutils
 import sys
 from functions import *
 
+
+def help_(args) -> None :
+    '''Prints the given function's repr'''
+    try:
+        doc = func_dict[args[0]].__doc__
+        print(doc if doc else "No documentation provided for given function")
+
+    except IndexError:
+        print("Please provide util name. run '$python3 cliutils.py list' to get a list of available utils")
+
+    except KeyError:
+        print("No such util. run '$python3 cliutils.py list' to get a list of available utils")
+
+def list_functs(args) -> None:
+    '''Prints a list of available functions. usage: '$python3 cliutils.py list'. Flags: -m for printing function repr with it.'''
+    
+    if "-m" in args:
+        
+        for key in func_dict:
+            print(f"{key}: {func_dict[key].__doc__}")
+
+    else:
+        print("\n".join([key for key in func_dict]).lstrip("\n"))
+
+def _m(args) -> None:
+    '''Prints -m help page'''
+    raise NotImplementedError("Sucks to suck ill implement this later :>")
+
+
 def handle_args() -> None:
     '''insert doc later'''
 
@@ -31,7 +60,13 @@ def handle_args() -> None:
         print("Please provide proper arguments.")
         return
 
+inbuilt_utils = {
+    "help":help_,
+    "list":list_functs,
+    "-m": _m,
+}
 
+func_dict.update(inbuilt_utils)
 
 if __name__ == "__main__":
 
--- functions.py
+++ functions.py
@@ -2,35 +2,9 @@
 from random import choice
 
 
-def help_(args) -> None :
-    '''Prints the given function's repr'''
-    try:
-        doc = func_dict[args[0]].__doc__
-        print(doc if doc else "No documentation provided for given function")
-
-    except IndexError:
-        print("Please provide util name. run '$python3 cliutils.py list' to get a list of available utils")
-
-    except KeyError:
-        print("No such util. run '$python3 cliutils.py list' to get a list of available utils")
-
-def list_functs(args) -> None:
-    '''Prints a list of available functions. usage: '$python3 cliutils.py list'. Flags: -m for printing function repr with it.'''
-    
-    if "-m" in args:
-        
-        for key in func_dict:
-            print(f"{key}: {func_dict[key].__doc__}")
-
-    else:
-        print("\n".join([key for key in func_dict]).lstrip("\n"))
-
-def _m(args) -> None:
-    '''Prints -m help page'''
-    raise NotImplementedError("Sucks to suck ill implement this later :>")
 
 #Example functions
-#Make sure function accepts only one str parameter
+#Make sure function accepts only one list parameter
 
 def upp(args) -> None :
     '''Converts to uppercase. usage: $python3 cliutils.py upper "<Text here>"'''
@@ -66,12 +40,8 @@ def randomcase(args):
 
 #Add your function to the dict
 func_dict = {
-    "help":help_,
-    "-m": _m,
     "upper":upp,
     "lower":lowe,
-    "list":list_functs,
     "invert":invert,
     "randomcase":randomcase
     }
-
